db9d94b97d20a7b0fd468ce2604c5bcdd14aaed4
Amir55143 <aamirnoor55143@gmail.com>
2023-09-21 15:06:39 +0500

    CheckBox

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _selectedBox = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('CheckBox'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Checkbox(
+              checkColor: Colors.amber,
+              activeColor: Colors.grey,
+              value: _selectedBox , 
+              onChanged: (box) {
+                setState(() {
+                    _selectedBox = box!;
+                });
+              }
+              ),
+            title: const Text('Flutter Development'),  
+          ),
+         ListTile(
+            leading: Checkbox(
+              value: _selectedBox , 
+              onChanged: (box) {
+                setState(() {
+                    _selectedBox = box!;
+                });
+              
+              }
+              ),
+            title: const Text('React Development'),  
+          ),
+          ListTile(
+            leading: Checkbox(
+              value: _selectedBox , 
+              onChanged: (box) {
+               setState(() {
+                    _selectedBox = box!;
+                });
+              }
+              ),
+            title: const Text('Web Development'),  
+          ), 
+        ],
+      ),
+    );
+  }
+}
